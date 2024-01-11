14330ba4d46814133b9252dceb81386d8a79f45e
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2024-01-11 22:17:40 +0700

    update design layout for halaman recap

--- app/src/main/java/com/example/mieyummy/halaman/HalamanRecap.kt
+++ app/src/main/java/com/example/mieyummy/halaman/HalamanRecap.kt
@@ -1,41 +1,54 @@
 package com.example.mieyummy.halaman
 
+import android.graphics.Color
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mieyummy.R
-import com.example.mieyummy.model.CreateViewModel
-import com.example.mieyummy.model.PenyediaViewModel
+import com.example.mieyummy.data.MieYummy
+import com.example.mieyummy.halaman.DestinasiLanding.LandingScreen
 import com.example.mieyummy.navigasi.DestinasiNavigasi
 import com.example.mieyummy.ui.theme.MieYummyTheme
 
-object DestinasiRecap: DestinasiNavigasi {
-    override val route= "RECAP"
-    override val titleRes= R.string.app_name
+object DestinasiRecap : DestinasiNavigasi {
+    override val route = "RECAP"
+    override val titleRes = R.string.app_name
 }
+
 @Composable
 fun RecapScreen(
-    navigateBack: ()-> Unit,
-    viewModel: CreateViewModel = androidx.lifecycle.viewmodel.compose.viewModel(factory = PenyediaViewModel.factory)
-){
-    val id: Int = 0
-    val jenis: String =""
-    val nama: String =""
-    val deskripsi: String =""
-    val harga: String =""
-
+    navigateBack: () -> Unit,
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -43,44 +56,45 @@ fun RecapScreen(
     )
     {
         Text(
-            text = "Pesanan Anda",
+            text = "Pesanan Saya",
             fontSize = 30.sp,
             fontWeight = FontWeight.Bold
         )
-        ElevatedCard (
-            elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
-            modifier = Modifier.fillMaxWidth()
-        ){
-            Text(
-                text = "ID :" + id,
-                fontSize = 16.sp,
-                modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp)
-            )
-            Text(
-                text = "Jenis :" + jenis,
-                fontSize = 16.sp,
-                modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp)
-            )
-            Text(
-                text = "Nama :" + nama,
-                fontSize = 16.sp,
-                modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp)
-            )
-            Text(
-                text = "Deskripsi Pesanan :" + deskripsi,
-                fontSize = 16.sp,
-                modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp)
-            )
-            Text(
-                text = "Harga :" + harga,
-                fontSize = 16.sp,
-                modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp)
-            )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Card(
+                modifier = Modifier,
+                elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+                    verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+                ) {
+                    Text(
+                        text = "Nama Menu : ",
+                        style = MaterialTheme.typography.titleMedium,
+                    )
+                    Text(
+                        text = "Deskripsi Menu :",
+                        style = MaterialTheme.typography.titleMedium,
+                    )
+                    Text(
+                        text = "Harga :",
+                        style = MaterialTheme.typography.titleMedium,
+                    )
+                }
+            }
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewHalamanRecap() {
-    MieYummyTheme {RecapScreen(navigateBack = {})    }
+    MieYummyTheme {
+        RecapScreen(navigateBack = {})
+    }
 }
