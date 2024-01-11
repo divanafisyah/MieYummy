d693d8ab90da1870f4cac4a0f9be427f9745c1d9
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2024-01-12 04:15:25 +0700

    final landing page

--- app/src/main/java/com/example/mieyummy/halaman/HalamanLanding.kt
+++ app/src/main/java/com/example/mieyummy/halaman/HalamanLanding.kt
@@ -1,6 +1,8 @@
 package com.example.mieyummy.halaman
 
+import android.graphics.Color
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,105 +33,97 @@ import com.example.mieyummy.halaman.DestinasiLanding.LandingScreen
 import com.example.mieyummy.navigasi.DestinasiNavigasi
 import com.example.mieyummy.ui.theme.MieYummyTheme
 
-object DestinasiLanding: DestinasiNavigasi {
+object DestinasiLanding : DestinasiNavigasi {
     override val route = "LANDING"
     override val titleRes = R.string.app_name
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun LandingScreen(
-        onMenuCustClick:()-> Unit,
-        onLoginClick:() -> Unit,
+        onMenuCustClick: () -> Unit,
+        onLoginClick: () -> Unit,
         modifier: Modifier = Modifier,
 
         ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxSize()
-        )
-        {
-            Spacer(modifier = Modifier.height(100.dp)
+            modifier = Modifier
+                .fillMaxSize()
+        ){
+            Image(
+                painter = painterResource(R.drawable.mieyummy_img),
+                contentDescription = "top",
+                modifier = Modifier.size(400.dp)
             )
-            Text(
-                text = "WELCOME TO MIE YUMMY!",
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
-            )
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(dimensionResource(R.dimen.padding_medium))
-                    .weight(1f), // Allow buttons to expand evenly
-                horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
-                verticalAlignment = Alignment.Bottom
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
             ) {
-                Column(
-                    modifier = Modifier,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
+                Button(
+                    onClick = onLoginClick,
+                    modifier = Modifier
+                        .width(150.dp)
+                        .height(50.dp)
+                        .align(Alignment.CenterHorizontally)
                 ) {
-                    Button(
-                        onClick = onLoginClick,
-                        modifier = Modifier
-                            .width(350.dp)
-                            .height(75.dp)
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center,
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Image(
-                                painter = painterResource(R.drawable.user),
-                                contentDescription = "Admin",
-                                modifier = Modifier.size(30.dp)
-                            )
-                            Spacer(modifier = Modifier.width(8.dp))
-                            Text(
-                                text = "ADMIN",
-                                fontSize = 30.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                        }
+                        Image(
+                            painter = painterResource(R.drawable.user),
+                            contentDescription = "Admin",
+                            modifier = Modifier.size(20.dp)
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            text = "ADMIN",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold
+                        )
                     }
-                    Spacer(modifier = Modifier.height(100.dp)
-                    )
-                    Button(
-                        onClick = onMenuCustClick,
-                        modifier = Modifier
-                            .width(350.dp)
-                            .height(75.dp)
+                }
+                Spacer(
+                    modifier = Modifier.height(20.dp)
+                )
+                Button(
+                    onClick = onMenuCustClick,
+                    modifier = Modifier
+                        .width(200.dp)
+                        .height(50.dp)
+                        .align(Alignment.CenterHorizontally)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center,
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Image(
-                                painter = painterResource(R.drawable.menu_white),
-                                contentDescription = "Menu",
-                                modifier = Modifier.size(30.dp)
-                            )
-                            Spacer(modifier = Modifier.width(8.dp))
-                            Text(
-                                text = "PESAN DISINI",
-                                fontSize = 30.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                        }
+                        Image(
+                            painter = painterResource(R.drawable.menu_white),
+                            contentDescription = "Menu",
+                            modifier = Modifier.size(20.dp)
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            text = "PESAN DISINI",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold
+                        )
                     }
-                    Spacer(modifier = Modifier.height(250.dp)
-                    )
                 }
             }
+
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
-fun PreviewHalamanLanding(){
-    MieYummyTheme{
+fun PreviewHalamanLanding() {
+    MieYummyTheme {
         LandingScreen(onMenuCustClick = {}, onLoginClick = {})
     }
 }
