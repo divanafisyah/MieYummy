fa452ef27e02f42c853472d7da928135168efada
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2024-01-11 23:27:50 +0700

    add raw changes part ke-sekian

--- app/src/main/java/com/example/mieyummy/halaman/HalamanMenuCustomer.kt
+++ app/src/main/java/com/example/mieyummy/halaman/HalamanMenuCustomer.kt
@@ -3,42 +3,28 @@ package com.example.mieyummy.halaman
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mieyummy.R
 import com.example.mieyummy.data.MieYummy
@@ -52,7 +38,6 @@ object DestinasiMenuCust: DestinasiNavigasi {
     override val route= "MENU"
     override val titleRes= R.string.psn
 }
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -108,8 +93,6 @@ fun ListCustMenu(
         }
     }
 }
-
-
 @Composable
 fun CustMenu(
     mieYummy: MieYummy,
@@ -145,88 +128,10 @@ fun CustMenu(
         }
     }
 }
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//fun HalamanMenuCustomer(
-//    onMenuCust:()-> Unit,
-//    onDetailClick:(Int)->Unit={},
-//    onNextButtonClicked: () -> Unit,
-//    onCancelButtonClicked: () -> Unit,
-//    modifier: Modifier = Modifier
-//){
-//    var quantity by remember { mutableIntStateOf(0) }
-//
-//    Column (modifier = modifier,
-//        verticalArrangement = Arrangement.SpaceBetween){
-//        Column (modifier =
-//        Modifier.padding(dimensionResource(R.dimen.padding_medium))){
-//            IconButton(
-//                onClick = { if (quantity > 1) quantity-- },
-//                modifier = Modifier.size(40.dp),
-//            ) {
-//                Icon(
-//                    imageVector = ImageVector.vectorResource(id = R.drawable.minus),
-//                    contentDescription = "Decrement"
-//                )
-//            }
-//
-//            Text(
-//                text = quantity.toString(),
-//                fontWeight = FontWeight.Bold,
-//                fontSize = 16.sp
-//            )
-//
-//            IconButton(
-//                onClick = { quantity++ },
-//                modifier = Modifier.size(40.dp),
-//            ) {
-//                Icon(
-//                    imageVector = ImageVector.vectorResource(id = R.drawable.add),
-//                    contentDescription = "Increment"
-//                )
-//            }
-//            Divider(
-//                thickness = dimensionResource(R.dimen.padding_medium),
-//                modifier = Modifier.padding(bottom =
-//                dimensionResource(R.dimen.padding_medium)
-//                )
-//            )
-//            Row (modifier = Modifier
-//                .fillMaxWidth()
-//                .padding(dimensionResource(R.dimen.padding_medium))
-//                .weight(1f, false),
-//                horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
-//            ){
-//
-//            }
-//            Divider(
-//                thickness = dimensionResource(R.dimen.padding_medium),
-//                modifier = Modifier.padding(bottom =
-//                dimensionResource(R.dimen.padding_medium)
-//                )
-//            )
-//            Row(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .padding(dimensionResource(R.dimen.padding_medium))
-//                    .weight(1f, false),
-//                horizontalArrangement =
-//                Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
-//                verticalAlignment = Alignment.Bottom
-//            ){
-//                OutlinedButton(
-//                    modifier = Modifier.weight(1f),
-//                    onClick = onCancelButtonClicked)
-//                {
-//                    Text(stringResource(R.string.cancel))
-//                }
-//                Button(
-//                    modifier = Modifier.weight(1f),
-//                    onClick = onNextButtonClicked
-//                ){
-//                    Text(stringResource(R.string.next))
-//                }
-//            }
-//        }
-//    }
-//}
+@Preview(showBackground = true)
+@Composable
+fun PreviewMenuCustomerScreen(){
+    MieYummyTheme{
+        MenuCustomerScreen()
+    }
+}
